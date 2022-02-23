4249897debf583da1b72eb80da805c9e5b4314b3
gutakk <thiramate.dev@gmail.com>
2022-02-23 17:17:24 +0700

    refactor

--- producer.js
+++ producer.js
@@ -2,8 +2,6 @@ import { createClient } from 'redis';
 
 (async () => {
   const client = createClient({url: 'redis://localhost:46379/0'});
-
   client.on('error', (err) => console.log('Redis Client Error', err));
-
-  await client.eval("redis.call('xadd', 'mystream', '*', 'test', 'hello')", 0)
+  await client.eval("redis.call('xadd', 'mystream', '*', 'test', 'hello', 'hello', 'world')", 0)
 })();
