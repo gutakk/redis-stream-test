434b79e1dc09515c08fcaf64ee7c234f03fbbad4
gutakk <thiramate.dev@gmail.com>
2022-02-24 18:34:41 +0700

    Integrate

--- producer.js
+++ producer.js
@@ -1,7 +1,15 @@
-import { createClient } from 'redis';
+import { createClient } from "redis";
 
 (async () => {
-  const client = createClient({url: 'redis://localhost:46379/0'});
+  const client = createClient({url: "redis://localhost:46379/0"});
+
   client.on('error', (err) => console.log('Redis Client Error', err));
-  await client.eval("redis.call('xadd', 'mystream', '*', 'test', 'hello', 'hello', 'world')", 0)
+
+  await client.connect();
+
+  for(let i=0; i<10; i++) {
+    await client.xAdd('test-stream', '*', {
+      id: i
+    })
+  }
 })();
--- websocket-server.js
+++ websocket-server.js
@@ -1,12 +1,51 @@
+import { createClient, commandOptions } from 'redis';
 import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 })
+const client = createClient({url: "redis://localhost:46379/0"});
+await client.connect();
+
+try {
+  await client.xGroupCreate('test-stream', 'myconsumergroup', '0', {
+    MKSTREAM: true
+  });
+  console.log('Created consumer group.');
+} catch (e) {
+  console.log('Consumer group already exists, skipped creation.');
+}
 
 wss.on('connection', (ws) => {
-  ws.on('message', (data) => {
-    console.log('received:', data.toString());
-    ws.send(`message back to ${data.toString()}`);
+  ws.on('message', async (data) => {
+    const jsonParams = JSON.parse(data);
+    const result = await consume(jsonParams.streamName);
+    ws.send(result);
   });
 
   ws.send('connected to websocket server');
 });
+
+async function consume(streamName, id) {
+  try {
+    let response = await client.xReadGroup(
+      commandOptions({
+        isolated: true
+      }),
+      'myconsumergroup', 
+      'myconsumer', [{
+        key: streamName,
+        id: '>' // Next entry ID that no consumer in this group has read
+      }], {
+        COUNT: 1,
+        BLOCK: 5000
+      }
+    );
+
+    if (response) {
+      return JSON.stringify(response);
+    } else {
+      return null;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
