7977305a2e17ce0c2ade9d8e494afe48013e27d9
gutakk <thiramate.dev@gmail.com>
2022-03-14 13:19:06 +0700

    Update producer

--- producer.js
+++ producer.js
@@ -1,25 +1,36 @@
-import { createClient } from "redis";
-
-function getRandom (list) {
-  return list[Math.floor((Math.random()*list.length))];
-}
+import { createClient } from 'redis';
 
 (async () => {
-  const client = createClient({url: "redis://localhost:46379/0"});
+  const client = createClient({ url: 'redis://localhost:46379/0' });
 
   client.on('error', (err) => console.log('Redis Client Error', err));
 
   await client.connect();
-  const rand = ['A', 'B', 'C'];
+  const ids = [
+    'xxxxxxxx-xxxx-xxth-bkkx-856738600641',
+    'xxxxxxxx-xxxx-xxth-bkkx-750630114775',
+    'xxxxxxxx-xxxx-xxth-bkkx-738156777998',
+    'xxxxxxxx-xxxx-xxth-bkkx-266814562608',
+    'xxxxxxxx-xxxx-xxth-bkkx-135654505895',
+    'xxxxxxxx-xxxx-xxth-bkkx-75272938801',
+    'xxxxxxxx-xxxx-xxth-bkkx-605266131710',
+    'xxxxxxxx-xxxx-xxth-bkkx-682852500808',
+    'xxxxxxxx-xxxx-xxth-bkkx-583142591932'
+  ]
 
-  setInterval(async () => {
-    for (let i=1; i<=20; i++) {
-      const data = {
-        playerId: i,
-        type: getRandom(rand)
-      };
-      await client.xAdd('test-stream', '*', data);
-      console.log('added', data);
-    }
-  }, 2000);
+  // event_name:
+  // player_id
+  // connection_state
+  // connected_at
+  // disconnected_at
+  await Promise.all(ids.map((id) => client.xAdd('screen-status-event-stream', '*', {
+    event_name: 'PLAYER_CONNECTION_STATE',
+    player_ids: [id],
+    connection_state: 'online',
+    connected_at: new Date().toString(),
+    disconnected_at: null
+  })))
+
+
+  console.log('added');
 })();
